16862f7b2f98669ef1e64fafbf00690a738f1678
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-03-10 21:25:51 -0500

    shorten directory prompt

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,4 @@
+# shorten directory prompt
+function prompt {"PS " + (get-location).drive.name+":\...\"+ $( ( get-item $pwd ).Name ) +">"}
+# fnm config for managing nodejs versions
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
